e4b6a93a464d60cd40b05a143941424b357df6bd
Eric Bode <eric.bode@foundries.io>
2022-05-19 16:07:40 +0200

    day13: Added part 2
    Signed-off-by: Eric Bode <eric.bode@foundries.io>

--- day13/src/main/scala/ShuttleBus.scala
+++ day13/src/main/scala/ShuttleBus.scala
@@ -2,9 +2,7 @@ object ShuttleBus {
   def main(args: Array[String]) : Unit = {
     val source = scala.io.Source.fromResource("input")
     val lines = try source.getLines().toList finally source.close()
-    val timetable = new Timetable(lines.head.toInt, lines.last)
+    val timetable = new Timetable(lines.head)
     println(timetable.getShuttleBus)
   }
-
-
 }
--- day13/src/main/scala/Timetable.scala
+++ day13/src/main/scala/Timetable.scala
@@ -1,15 +1,23 @@
-class Timetable(departure: Int, timetable: String) {
-  val shuttles: Array[Int] = timetable
-    .split(",")
-    .filter(s => !s.equalsIgnoreCase("x"))
-    .map(s => s.toInt)
-  var service : Map[Int, Int] = Map.empty
+import scala.sys.exit
+import util.control.Breaks._
 
-  def getShuttleBus: Int = {
-    for (shuttle <- shuttles ) {
-      service += shuttle -> (shuttle - (departure % shuttle))
-    }
-    val min = service.minBy(kv => kv._2)
-    min._1 * min._2
+class Timetable(timetable: String) {
+  val shuttles: Array[(Long, Long)] = timetable
+    .split(",")
+    .zipWithIndex
+    .filter(s => !s._1.equalsIgnoreCase("x"))
+    .map(s => (s._1.toLong, s._2.toLong))
+    .sortBy(s => s._2)
+
+  def getShuttleBus: Long = {
+    var stepSize = shuttles.head._1
+    var time = 0L
+    shuttles.drop(1).foreach(f => {
+      while ((time + f._2) % f._1 != 0L) {
+        time += stepSize
+      }
+      stepSize *= f._1
+    })
+    return time
   }
 }
