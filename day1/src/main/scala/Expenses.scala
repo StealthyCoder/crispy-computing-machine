b7da4bf0c14c862e2053d45b0b8dbedd5f562aee
Eric Bode <eric.bode@foundries.io>
2022-05-19 16:25:37 +0200

    day1: Added part 1 back to it
    Signed-off-by: Eric Bode <eric.bode@foundries.io>

--- day1/src/main/scala/Expenses.scala
+++ day1/src/main/scala/Expenses.scala
@@ -4,6 +4,10 @@ object Expenses {
     val expenses = try source.getLines().map(s => s.toInt).toList finally source.close()
     expenses.foreach(expense => {
       val counterPart = 2020 - expense
+      expenses.find(p => p == counterPart).map(p => counterPart * expense).map(println)
+    })
+    expenses.foreach(expense => {
+      val counterPart = 2020 - expense
       expenses.foreach(other => {
         val thirdPart = counterPart - other
         if ( expenses.contains(thirdPart)) {
